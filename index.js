0d0d61826e40c1013720f545cf73e3ebd295c4f4
johnkiguma <154498415+johnkiguma@users.noreply.github.com>
2024-01-20 09:35:36 +0300

    completed

---
+++ index.js
@@ -0,0 +1,41 @@
+// const url ='https://docs.impala.travel/docs/booking-api/';
+// const options ={
+//   method :'GET' ,
+// }
+const db = "https://docs.impala.travel/docs/booking-api/"
+
+document.addEventListener("DOMContentLoaded", () => {
+    getHotels(data);
+    document.querySelector("#book-hotel").addEventListener("click", handleBookTicket);
+});
+
+function getHotels() {
+    fetch(db)
+    .then(res => res.json())
+    .then(hotel => {
+        hotel.forEach(hotel => {renderHotelList(hotel)})
+        const firstHotel = document.querySelector("#id1");
+        firstHotel.dispatchEvent(new Event("click"));
+    })
+}
+function handleHotelClick(hotel) {
+  const poster = document.querySelector("img#poster")
+  poster.src = hotel.poster;
+  poster.alt = hotel.title;
+  const info = document.querySelector("#showing");
+  info.querySelector("ln-list").textContent = hotel.contact;
+  // info.querySelector("#runtime").textContent = movie.runtime+" minutes";
+  info.querySelector("ln-hotel").textContent = hotel.description;
+  // info.querySelector("#showtime").textContent = movie.showtime;
+  info.querySelector("#ticket-num").textContent = movie.capacity - movie.tickets_sold + " remaining tickets";
+}
+function handleBookHotel() {
+  const ticketDiv = document.querySelector("#ticket-num");
+  const tickets = ticketDiv.textContent.split(" ")[0];
+  if (tickets > 0) {
+      ticketDiv.textContent = tickets - 1 + " remaining tickets";
+  }
+}
+function addEventListener(){
+  
+}
